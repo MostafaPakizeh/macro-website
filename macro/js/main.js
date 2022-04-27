ff2d547586a15a1fea871913840ec98a33f8dcff
Mostafa Pakizeh <83976978+MostafaPakizeh@users.noreply.github.com>
2022-04-27 23:31:40 +0430

    Add files via upload

---
+++ macro/js/main.js
@@ -0,0 +1,39 @@
+// $(document).ready(function () {
+//   $(".owl-carousel").owlCarousel({
+//     items: 3,
+//     loop: true,
+//     margin: 10,
+//     autoplay: true,
+//     autoplayTimeout: 1000,
+//     autoplayHoverPause: true,
+//   });
+// });
+
+// $(document).ready(function () {
+//   $(".owl-carousel").owlCarousel({
+//     rtl: true,
+//     loop: true,
+//     margin: 10,
+//     nav: true,
+//     responsive: {
+//       0: {
+//         items: 1,
+//       },
+//       600: {
+//         items: 3,
+//       },
+//       1000: {
+//         items: 5,
+//       },
+//     },
+//   });
+// });
+
+// $(document).ready(function() {
+//   $(".slider").slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
+// });
